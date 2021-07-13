c9af457f5210cc00e8a9a77d947311c3ddecf480
Yunho <potatohead12@naver.com>
2021-07-13 07:39:53 +0000

    1436백준(init)

---
+++ back1436.py
@@ -0,0 +1,22 @@
+N = input()
+l = len(N)
+n = int(N)
+
+import re
+
+p = re.compile('666')
+
+num = []
+
+demise = []
+x = 666
+while len(demise) < n:
+    for can in num:
+        if p.search(str(can)):
+            demise.append(can)
+    num=list(range(x,1000*l))
+    x = 1000*l
+    l+=1
+
+print(demise[n-1])
+    
